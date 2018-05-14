6bcc8a1e4413fd90dc29f4af66257de73e91f14e
Alex Kavanagh <alex@ajkavanagh.co.uk>
2018-05-14 11:17:19 +0100

    Add haskell pre-install script for stack tools

---
+++ .config/nvim/non-vim-install-scripts/haskell.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+# --- ENSURE stack is installed first.
+if [[ -z $(which stack) ]]; then
+	echo "You need to install stack first: https://docs.haskellstack.org/en/stable/README/"
+	exit 1
+fi
+
+# Install tools to support nvim coding
+# from: https://blog.jez.io/haskell-development-with-neovim/
+stack install hindent
+stack install stylish-haskell
+# it's old and this is the latest version that seems to work.
+stack build ghc-mod-5.7.0.0 --resolver=lts-8.23
