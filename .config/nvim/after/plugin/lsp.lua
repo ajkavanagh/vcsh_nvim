ef90c7bfb6e4f7a153de78b4a80c67dd452101e1
Alex Kavanagh <alex@ajkavanagh.co.uk>
2022-01-13 18:09:21 +0000

    Make the lsp config a bit more usable
    * Added <leader>ll to restart the LSP server.  It seems to crash a lot,
      so restarting it can clean up the buffer.
    * Stop the LSP diagnostics cluttering up the gutter; I'm keeping it for
      git changes, which is much more useful.  The LSP diagnostics will be
      via underlining.

--- .config/nvim/after/plugin/lsp.lua
+++ .config/nvim/after/plugin/lsp.lua
@@ -4,3 +4,6 @@ if not status_ok then
 end
 
 require('lsp_config.config')
+
+-- nnoremap <leader>c :ccl<CR>
+vim.api.nvim_set_keymap('n', '<Leader>ll', ':LspRestart<CR>', {noremap = true, silent = false})
--- .config/nvim/lua/lsp_config/config.lua
+++ .config/nvim/lua/lsp_config/config.lua
@@ -1,2 +1,7 @@
 require('lsp_config.lsp_installer')
 require('lsp_config.handlers').setup()
+
+
+vim.diagnostic.config({
+  signs = false,    -- I find diagnostics interferes with gutter for changes
+})
